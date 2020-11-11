6f2d02511a939b58c910e2bbee75c745f2e2ff2d
Marcel <m-gepraegs@web.de>
2020-11-12 00:43:06 +0100

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_flutter_test/todo_screen.dart';
 import 'login.dart';
 
 void main() {
   runApp(MaterialApp(home: ToDo()));
 }
 
-class ToDo extends StatefulWidget {
-  @override
-  _ToDoState createState() => _ToDoState();
-}
+class ToDo extends StatelessWidget {
 
-class _ToDoState extends State<ToDo> {
-  // @override
-  // void dispose() async {
-  //   await FirebaseAuth.instance.signOut();
-  //   print('User logged out!');
-  //   super.dispose();
+  // Widget createWidget() {
+  //   if (FirebaseAuth.instance.currentUser != null) {
+  //
+  //   }
   // }
+  User currentUser;
+
+  void initApp() async {
+    print('Init APP!');
+    await Firebase.initializeApp();
+    if (FirebaseAuth.instance.currentUser != null) {
+      currentUser = FirebaseAuth.instance.currentUser;
+      print('Current User: ' + FirebaseAuth.instance.currentUser.uid);
+    } else {
+      currentUser = null;
+      print('Current User is NULL!');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    initApp();
+    if (currentUser == null) {
+      return LoginScreen();
+    } else {
+      return ToDoScreen(currentUser);
+    }
   }
 }
